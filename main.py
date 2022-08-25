f98d7b1dd2f511d68e342b7646046dd6b84f77a5
Zaha_xk93@student.itstep.kz <Zaha_xk93@student.itstep.kz>
2022-08-25 09:57:41 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from modules.minus import minus
+from modules.multiplication import multiplication
+from modules.plus import plus
+from modules.division import division
+
+if __name__ == '__main__':
+    print(plus(1,2))
+    print(minus(1,2))
+    print(multiplication(1,2))
+    print(division(1,2))
